c1be6bcd6d7d1b100836cf8c54f7f198bf4599d4
shuupsy <sylvaine.ruan@gmail.com>
2022-10-05 11:15:14 +0200

    Webphp: routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,32 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoomsController;
+use App\Http\Controllers\StaffController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\GalleryController;
 
-
-/* Route::get('/', function () {
-    return view('pages.home');
-}); */
 
 Route::resource('/', HomeController::class)
     ->names(['index' => 'home.index']);
+
+Route::resource('/rooms', RoomsController::class)
+    ->names(['index' => 'rooms.index']);
+
+Route::resource('/staff', StaffController::class)
+    ->names(['index' => 'staff.index']);
+
+Route::resource('/gallery', GalleryController::class)
+    ->names(['index' => 'galley.index']);
+
+Route::resource('/contact', ContactController::class)
+    ->names(['index' => 'contact.index']);
+
+Route::resource('/blog', BlogController::class)
+    ->names(['index' => 'blog.index']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
