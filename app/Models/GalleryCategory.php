29e52ffc626d0e7be585ebc806f204adca649595
shuupsy <sylvaine.ruan@gmail.com>
2022-10-05 10:32:56 +0200

    DB: galleryphoto/cat (function)

--- app/Models/GalleryCategory.php
+++ app/Models/GalleryCategory.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\GalleryPhoto;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class GalleryCategory extends Model
 {
     use HasFactory;
+
+    public function photo(){
+        return $this->hasMany(GalleryPhoto::class);
+    }
 }
--- app/Models/GalleryPhoto.php
+++ app/Models/GalleryPhoto.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\GalleryCategory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class GalleryPhoto extends Model
 {
     use HasFactory;
+
+    public function category(){
+        return $this->belongsTo(GalleryCategory::class);
+    }
 }
