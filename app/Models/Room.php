86f4a171961808c9a0b04bdbc7ed46eba9df0bc9
shuupsy <sylvaine.ruan@gmail.com>
2022-10-05 10:03:05 +0200

    DB: RoomTag (pivot table, function)

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Tag;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Room extends Model
 {
     use HasFactory;
+
+    public function tag(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
