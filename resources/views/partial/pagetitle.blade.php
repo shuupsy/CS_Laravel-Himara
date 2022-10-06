b92b3c29beb2766f8b94326bc79ec3fe8a4b37c7
shuupsy <sylvaine.ruan@gmail.com>
2022-10-06 16:37:10 +0200

    Pagetitle: modif titre selon la page

--- resources/views/partial/pagetitle.blade.php
+++ resources/views/partial/pagetitle.blade.php
@@ -1,15 +1,21 @@
 <!-- ========== PAGE TITLE ========== -->
-<div class="page-title gradient-overlay op6" style="background: url(images/breadcrumb.jpg); background-repeat: no-repeat;
+<div class="page-title gradient-overlay op6"
+    style="background: url(images/breadcrumb.jpg); background-repeat: no-repeat;
 background-size: cover;">
-  <div class="container">
-    <div class="inner">
-      <h1>ROOMS</h1>
-      <ol class="breadcrumb">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li class='capitalize'>{{ substr_replace(ucfirst(Route::current()->getName()), "", -6) }}</li>
-      </ol>
+    <div class="container">
+        <div class="inner">
+            @if (request()->is('staff'))
+                <h1>Our Staff</h1>
+            @else
+                <h1>{{ substr_replace(ucfirst(Route::current()->getName()), '', -6) }}</h1>
+            @endif
+
+            <ol class="breadcrumb">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li class='capitalize'>{{ substr_replace(ucfirst(Route::current()->getName()), '', -6) }}</li>
+            </ol>
+        </div>
     </div>
-  </div>
 </div>
