2a280aee4a148c484e388967e08b690f036e4af5
shuupsy <sylvaine.ruan@gmail.com>
2022-10-10 09:52:21 +0200

    DB : User, new column (firstname/lastname)

--- database/migrations/2022_10_12_000000_create_users_table.php
+++ database/migrations/2022_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
             $table->foreignId('role_id')
                 ->default(4)
                 ->constrained();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
