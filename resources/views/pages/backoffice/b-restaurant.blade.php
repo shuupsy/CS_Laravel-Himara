4929ceaeda587a07674ca584917f4a0818c9344d
shuupsy <sylvaine.ruan@gmail.com>
2022-10-13 15:30:51 +0200

    Restaurant : blade mise en forme update

--- resources/views/pages/backoffice/b-restaurant.blade.php
+++ resources/views/pages/backoffice/b-restaurant.blade.php
@@ -4,12 +4,12 @@
 @endsection
 
 @section('update')
-    <div class='flex flex-col gap-10'>
+    <div class='grid grid-cols-3 gap-10'>
         @foreach ($dishes as $dish)
             <div class="p-6 border-b bg-white border-gray-200">
-                <h1 class='text-2xl text-[#E4E4E4] my-1 uppercase font-semibold'>{{ $dish->title }}</h1>
-                <div class='w-3/6 flex gap-6'>
-                    <div class='w-2/4 object-fit'>
+                <h1 class='text-2xl text-[#D8BA8D] my-1 uppercase font-semibold'>{{ $dish->title }}</h1>
+                <div class='flex flex-col gap-1'>
+                    <div class='object-fit flex justify-center'>
                         @if (Str::startsWith($dish->photo, 'https:'))
                             <img src="{{ $dish->photo }}">
                         @else
@@ -17,47 +17,47 @@
                         @endif
                     </div>
 
-                    <div>
-                        <form action="/admin/dishs/{{ $dish->id }}" method='post' enctype="multipart/form-data">
-                            @csrf
-                            @method('patch')
+                    <form action="/admin/dishs/{{ $dish->id }}" method='post' enctype="multipart/form-data">
+                        @csrf
+                        @method('patch')
 
-                            <div class='flex flex-col gap-3 my-6'>
-                                <!-- Title -->
-                                <div>
-                                    <x-input-label for="title" :value="__('Title')" />
+                        <div class='flex flex-col gap-3 my-6'>
+                            <!-- Title -->
+                            <div>
+                                <x-input-label for="title" :value="__('Title')" />
 
-                                    <x-text-input id="title" class="block mt-1 w-72" type="text" name="title"
-                                        :value="$dish->title" required autofocus />
-                                </div>
-
-                                <!-- Description -->
-                                <div>
-                                    <x-input-label for="description" :value="__('Description')" />
-
-                                    <textarea id="description" class="block mt-1 w-72" type="text" name="description" value="{{ old('description') }}"
-                                        required autofocus>{{ $dish->description }}</textarea>
-                                </div>
-
-                                <!-- Image -->
-                                <div>
-                                    <x-input-label for="image" :value="__('Image')" />
-
-                                    <x-text-input id="image" type="file" name="image" autofocus />
-                                </div>
-
-
+                                <x-text-input id="title" class="block mt-1 w-72" type="text" name="title"
+                                    :value="$dish->title" required autofocus />
                             </div>
 
-                            <button class='bg-[#444444] p-2 text-white rounded-sm hover:bg-[#222222]'>Update</button>
-                        </form>
+                            <!-- Description -->
+                            <div>
+                                <x-input-label for="description" :value="__('Description')" />
 
-                        <form action="/admin/dishs/{{ $dish->id }}" method='post'>
-                            @csrf
-                            @method('delete')
-                            <button class='text-red-600 rounded-sm p-2 hover:underline'>delete</button>
-                        </form>
-                    </div>
+                                <textarea id="description"
+                                    class="block mt-1 w-72 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                    type="text" name="description" value="{{ old('description') }}" required autofocus>{{ $dish->description }}</textarea>
+                            </div>
+
+                            <!-- Image -->
+                            <div>
+                                <x-input-label for="image" :value="__('Image')" />
+
+                                <x-text-input id="image" type="file" name="image" autofocus />
+                            </div>
+
+
+                        </div>
+
+                        <button class='bg-[#444444] p-2 text-white rounded-sm hover:bg-[#222222]'>Update</button>
+                    </form>
+
+                    <form action="/admin/dishs/{{ $dish->id }}" method='post'>
+                        @csrf
+                        @method('delete')
+                        <button class='text-red-600 rounded-sm p-2 hover:underline'>delete</button>
+                    </form>
+
 
                 </div>
 
