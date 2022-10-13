a9d3e7c7fe99b10b8c8b53d89494563b88df053f
shuupsy <sylvaine.ruan@gmail.com>
2022-10-13 15:22:18 +0200

    Restaurant: affichage si lien https (aliases)

--- config/app.php
+++ config/app.php
@@ -213,7 +213,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Str' => \Illuminate\Support\Str::class,
     ])->toArray(),
 
 ];
--- resources/views/pages/backoffice/b-restaurant.blade.php
+++ resources/views/pages/backoffice/b-restaurant.blade.php
@@ -1,62 +1,67 @@
 @extends('layouts.app')
 
 @section('new')
-
 @endsection
 
 @section('update')
-<div class='flex flex-col gap-10'>
-    @foreach ($dishes as $dish)
-        <div class="p-6 border-b bg-white border-gray-200">
-            <h1 class='text-2xl text-[#E4E4E4] my-1 uppercase font-semibold'>{{ $dish->title }}</h1>
-            <div class='w-3/6 flex gap-6'>
-                <img src="/images/restaurant/{{ $dish->photo }}">
+    <div class='flex flex-col gap-10'>
+        @foreach ($dishes as $dish)
+            <div class="p-6 border-b bg-white border-gray-200">
+                <h1 class='text-2xl text-[#E4E4E4] my-1 uppercase font-semibold'>{{ $dish->title }}</h1>
+                <div class='w-3/6 flex gap-6'>
+                    <div class='w-2/4 object-fit'>
+                        @if (Str::startsWith($dish->photo, 'https:'))
+                            <img src="{{ $dish->photo }}">
+                        @else
+                            <img src="/images/restaurant/{{ $dish->photo }}">
+                        @endif
+                    </div>
 
-                <div>
-                    <form action="/admin/dishs/{{ $dish->id }}" method='post' enctype="multipart/form-data">
-                        @csrf
-                        @method('patch')
+                    <div>
+                        <form action="/admin/dishs/{{ $dish->id }}" method='post' enctype="multipart/form-data">
+                            @csrf
+                            @method('patch')
 
-                        <div class='flex flex-col gap-3 my-6'>
-                            <!-- Heading -->
-                            <div>
-                                <x-input-label for="layer1" :value="__('Heading')" />
+                            <div class='flex flex-col gap-3 my-6'>
+                                <!-- Title -->
+                                <div>
+                                    <x-input-label for="title" :value="__('Title')" />
 
-                                <x-text-input id="layer1" class="block mt-1 w-72" type="text" name="layer1"
-                                    :value="$dish->layer1" required autofocus />
+                                    <x-text-input id="title" class="block mt-1 w-72" type="text" name="title"
+                                        :value="$dish->title" required autofocus />
+                                </div>
+
+                                <!-- Description -->
+                                <div>
+                                    <x-input-label for="description" :value="__('Description')" />
+
+                                    <textarea id="description" class="block mt-1 w-72" type="text" name="description" value="{{ old('description') }}"
+                                        required autofocus>{{ $dish->description }}</textarea>
+                                </div>
+
+                                <!-- Image -->
+                                <div>
+                                    <x-input-label for="image" :value="__('Image')" />
+
+                                    <x-text-input id="image" type="file" name="image" autofocus />
+                                </div>
+
+
                             </div>
 
-                            <!-- Subheading -->
-                            <div>
-                                <x-input-label for="layer2" :value="__('Subheading')" />
+                            <button class='bg-[#444444] p-2 text-white rounded-sm hover:bg-[#222222]'>Update</button>
+                        </form>
 
-                                <x-text-input id="layer2" class="block mt-1 w-72" type="text" name="layer2"
-                                    :value="$dish->layer2" required autofocus />
-                            </div>
+                        <form action="/admin/dishs/{{ $dish->id }}" method='post'>
+                            @csrf
+                            @method('delete')
+                            <button class='text-red-600 rounded-sm p-2 hover:underline'>delete</button>
+                        </form>
+                    </div>
 
-                            <!-- Image -->
-                            <div>
-                                <x-input-label for="image" :value="__('Image')" />
-
-                                <x-text-input id="image" type="file" name="image" autofocus />
-                            </div>
-
-
-                        </div>
-
-                        <button class='bg-[#444444] p-2 text-white rounded-sm hover:bg-[#222222]'>Update</button>
-                    </form>
-
-                    <form action="/admin/dishs/{{ $dish->id }}" method='post'>
-                        @csrf
-                        @method('delete')
-                        <button class='text-red-600 rounded-sm p-2 hover:underline'>delete</button>
-                    </form>
                 </div>
 
             </div>
-
-        </div>
-    @endforeach
-</div>
+        @endforeach
+    </div>
 @endsection
