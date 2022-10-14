439823f555959b10fa7085a1da1674330cde2be9
shuupsy <sylvaine.ruan@gmail.com>
2022-10-14 14:05:00 +0200

    Services: store

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Logo;
 use App\Models\Service;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Storage;
 
@@ -42,7 +44,25 @@ class ServiceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $new_service = new Service();
+
+        /* Image */
+        Storage::put('public/assets/', $request->file('image'));
+
+        $new = $request->file('image')->hashName();
+        $new_path = public_path('storage/assets/' . $new);
+        $resize = Image::make($new_path)->resize(1170, 750)->save(public_path('images/services/' . $new));
+
+        $new_service -> image = $new;
+
+        /* Infos */
+        $new_service -> title = $request -> title;
+        $new_service -> description = $request -> description;
+        $new_service -> logo_id = $request -> logo;
+
+        $new_service->save();
+
+        return redirect()->back()->with('success', '(1) Service ajouté avec succès!');
     }
 
     /**
@@ -108,6 +128,14 @@ class ServiceController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $delete = Service::find($id);
+
+        Storage::delete('public/assets/' . $delete->photo);
+        File::delete(public_path('images/service/' . $delete->photo));
+
+
+        $delete->delete();
+
+        return redirect()->back()->with('success', '(1) Service supprimé avec succès!');
     }
 }
--- resources/views/pages/backoffice/b-service.blade.php
+++ resources/views/pages/backoffice/b-service.blade.php
@@ -1,42 +1,113 @@
 @extends('layouts.app')
 
-@section('preview')
-    <div class="services h-96 p-6 border-b bg-white border-gray-200">
-        <div class="grid grid-cols-2 h-max">
-            {{-- Carousel - Images --}}
-            <div data-slider-id="services" class="services-owl owl-carousel">
-                @foreach ($services as $service)
-                    <figure class="gradient-overlay">
-                        <img src="/images/services/{{ $service->image }}" class="img-fluid" alt="Image">
-                        <figcaption>
-                            <h4>{{ $service->title }}</h4>
-                        </figcaption>
-                    </figure>
-                @endforeach
-            </div>
+@if ($count != 0)
+    @section('preview')
+        <div class="services h-[30rem] p-6 border-b bg-white border-gray-200">
+            <div class="grid grid-cols-2 h-max">
+                {{-- Carousel - Images --}}
+                <div data-slider-id="services" class="services-owl owl-carousel">
+                    @foreach ($services as $service)
+                        <figure class="gradient-overlay">
+                            <img src="/images/services/{{ $service->image }}" class="img-fluid" alt="Image">
+                            <figcaption>
+                                <h4>{{ $service->title }}</h4>
+                            </figcaption>
+                        </figure>
+                    @endforeach
+                </div>
 
-            {{-- Carousel - Description --}}
-            <div class="owl-thumbs" data-slider-id="services">
-                @foreach ($services as $service)
-                    <div class="owl-thumb-item">
-                        <span class="media-left">
-                            <i class="{{ $service->logo->logo }}"></i>
-                        </span>
-                        <div class="media-body">
-                            <h5>{{ $service->title }}</h5>
-                            <p>{{ $service->description }}</p>
+                {{-- Carousel - Description --}}
+                <div class="owl-thumbs" data-slider-id="services">
+                    @foreach ($services as $service)
+                        <div class="owl-thumb-item">
+                            <span class="media-left">
+                                <i class="{{ $service->logo->logo }}"></i>
+                            </span>
+                            <div class="media-body">
+                                <h5>{{ $service->title }}</h5>
+                                <p>{{ $service->description }}</p>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
+
             </div>
-
         </div>
-    </div>
-@endsection
+    @endsection
+@endif
 
-@section('new')
-    ALLO
-@endsection
+
+@if ($count == 4)
+    @section('new')
+        <div class='flex flex-col gap-10'>
+            <div class="p-6 border-b bg-white border-gray-200">
+                <h1 class='text-2xl text-[#D8BA8D] my-1 uppercase font-semibold'>
+                    Nombre de services maximum atteint !
+                </h1>
+                <h2 class='text-lg my-1'>
+                    Veuillez supprimer un service avant de procéder.
+                </h2>
+            </div>
+        </div>
+    @endsection
+@else
+    @section('new')
+        <div class='flex flex-col gap-10'>
+            <div class="p-6 border-b bg-white border-gray-200">
+                <form action="/admin/services" method='post' enctype="multipart/form-data">
+                    @csrf
+
+                    <div class='flex gap-6 items-between'>
+                        <div class='w-5/12 flex flex-col'>
+                            <img src="/images/services/service-example.png" alt="Service Photo">
+                            <!-- Image -->
+                            <x-text-input id="image" type="file" name="image" autofocus />
+                        </div>
+
+                        <!-- Choix de Logo -->
+                        <div>
+                            <x-input-label for="logo" :value="__('Logo')" />
+                            <div class='grid grid-cols-5 gap-3'>
+                                @foreach ($logos as $logo)
+                                    <label class='flex flex-col items-center gap-1'>
+                                        <i class="{{ $logo->logo }} m-0 text-[#D8BA8D] text-4xl border px-2"></i>
+
+                                        <input type="radio" name="logo" value="{{ $logo->id }}" id="logo"
+                                            {{ $loop->index == 0 ? 'checked' : '' }}>
+                                    </label>
+                                @endforeach
+                            </div>
+
+                        </div>
+
+                        <div class='flex flex-col gap-4'>
+                            <!-- Title-->
+                            <div>
+                                <x-input-label for="title" :value="__('Title')" />
+
+                                <x-text-input id="title" class="block mt-1 w-72" type="text" name="title"
+                                    :value="old('title')" required autofocus />
+                            </div>
+
+                            <!-- Description -->
+                            <div>
+                                <x-input-label for="description" :value="__('Description')" />
+
+                                <textarea id="description"
+                                    class="block mt-1 w-72 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                    type="text" name="description" value="{{ old('description') }}" required autofocus></textarea>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <button class='bg-[#444444] p-2 text-white rounded-sm hover:bg-[#222222] mt-3 uppercase'>Add</button>
+                </form>
+            </div>
+        </div>
+    @endsection
+@endif
+
 
 @if ($count != 0)
     @section('update')
@@ -51,17 +122,22 @@
                         @method('patch')
 
                         <div class='flex gap-6 items-between'>
-                            <img src="/images/services/{{ $service->image }}" alt="Service Photo" width='420'>
+                            <div class='w-5/12 flex flex-col'>
+                                <img src="/images/services/{{ $service->image }}" alt="Service Photo">
+                                <!-- Image -->
+                                <x-text-input id="image" type="file" name="image" autofocus />
+                            </div>
 
                             <!-- Choix de Logo -->
                             <div>
                                 <x-input-label for="logo" :value="__('Logo')" />
-                                <div class='flex flex-wrap gap-3'>
+                                <div class='grid grid-cols-5 gap-3'>
                                     @foreach ($logos as $logo)
                                         <label class='flex flex-col items-center gap-1'>
                                             <i class="{{ $logo->logo }} m-0 text-[#D8BA8D] text-4xl border px-2"></i>
 
-                                            <input type="radio" name="logo" value="{{ $logo->id }}" id="logo" {{ $service->logo->id == $logo->id ? 'checked' : '' }}>
+                                            <input type="radio" name="logo" value="{{ $logo->id }}" id="logo"
+                                                {{ $service->logo->id == $logo->id ? 'checked' : '' }}>
                                         </label>
                                     @endforeach
                                 </div>
@@ -88,10 +164,6 @@
                             </div>
 
                         </div>
-                        <!-- Image -->
-                        <x-text-input id="image" type="file" name="image" autofocus />
-
-                        <br>
 
                         <button class='bg-[#444444] p-2 text-white rounded-sm hover:bg-[#222222] mt-3'>Update</button>
                     </form>
