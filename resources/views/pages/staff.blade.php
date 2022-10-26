00eba16524d97ef4e7a1fd1ce03bc5de7162c8ec
shuupsy <sylvaine.ruan@gmail.com>
2022-10-26 11:14:12 +0200

    Staff : même taille

--- resources/views/pages/staff.blade.php
+++ resources/views/pages/staff.blade.php
@@ -8,29 +8,38 @@
                 <div class="col-lg-3 col-md-6">
                     <div class="staff-item">
                         <figure>
-                            <img src="images/staff/staff1.jpg" class="img-fluid" alt="Image">
-                            <div class="position">{{ $boss -> job }}</div>
+                            @if (Str::startsWith($boss->photo, 'https:'))
+                            <img src="{{ $boss->photo }}" class="img-fluid" alt="Image" width='640'>
+                        @else
+                            <img src="/images/staff/{{ $boss->photo }}" class="img-fluid" alt="Image">
+                        @endif
+                            <div class="position">{{ $boss->job }}</div>
                         </figure>
                         <div class="details">
-                            <h5>{{ $boss -> first_name }} {{ $boss-> last_name }}</h5>
-                            <p>{{ $boss -> description}}</p>
+                            <h5>{{ $boss->first_name }} {{ $boss->last_name }}</h5>
+                            <p>{{ $boss->description }}</p>
                         </div>
                     </div>
                 </div>
                 <!-- Members -->
                 @foreach ($staffmembers as $member)
-                <div class="col-lg-3 col-md-6">
-                    <div class="staff-item">
-                        <figure>
-                            <img src="images/staff/staff1.jpg" class="img-fluid" alt="Image">
-                            <div class="position">{{ $member -> job }}</div>
-                        </figure>
-                        <div class="details">
-                            <h5>{{ $member -> first_name }} {{ $member-> last_name }}</h5>
-                            <p>{{ $member -> description}}</p>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="staff-item">
+                            <figure>
+                                @if (Str::startsWith($member->photo, 'https:'))
+                                    <img src="{{ $member->photo }}" class="img-fluid" alt="Image" width='640'>
+                                @else
+                                    <img src="/images/staff/{{ $member->photo }}" class="img-fluid" alt="Image">
+                                @endif
+                                
+                                <div class="position">{{ $member->job }}</div>
+                            </figure>
+                            <div class="details">
+                                <h5>{{ $member->first_name }} {{ $member->last_name }}</h5>
+                                <p>{{ $member->description }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
 
             </div>
